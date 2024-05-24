4b8d583d2f961dd8bb6db948a079630b80157ed5
juanruizgalileo <115655920+juanruizgalileo@users.noreply.github.com>
2024-05-23 19:47:17 -0600

    Create Precalificacion.php

---
+++ Precalificacion.php
@@ -0,0 +1,46 @@
+<?php
+
+class Calculadora {
+
+    public $listaNegra = array(
+        "1234567801010",
+        "2345678901011",
+        "3456789001012",
+        "4567890101013",
+        "5678901201014",
+        "6789012301015",
+        "7890123401016",
+        "8901234501017",
+        "9012345601018",
+        "0123456701019",
+        "1123456801010",
+        "2234567901011",
+        "3345678001012",
+        "4456789101013",
+        "5567890201014",
+        "6678901301015",
+        "7789012401016",
+        "8890123501017",
+        "9901234601011",
+        "0912345701013"
+    );
+
+
+    public function cuiValido($dpi) {
+        
+        return false;
+    }
+    
+    public function cuiSinProblemas($dpi) {
+        
+        return false;
+    }
+    
+    public function nivelEndeudamientoValido($ingreso_mensual, $endeudamiento_mensual, $cuota_mensual) {
+        
+        return false;
+    }
+
+}
+
+?>
